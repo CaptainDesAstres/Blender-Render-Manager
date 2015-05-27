19190052766b6f42f45e1d9d077b0a32bf527734
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 15:33:52 +0200

    create blank log file on first running

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 # -*-coding:Utf-8 -*
 
 import os
-
+prelog = 'ouverture de Blender Render Manager\n'
 #aller dans le dossier de configuration de Blender Render Manager
 if not os.path.exists('/home/'+os.getlogin()+'/.BlenderRenderManager'):
+	prelog += 'No configuration directorie, create it: fail'
 	os.mkdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
+	prelog = prelog[:len(prelog)-4]+'done\n'
+else:
+	prelog += 'Find configuration directorie\n'
 os.chdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
 
 #vérifier l'existance du dossier des logs et création si necessaire:
 if not os.path.exists(os.getcwd()+'/log'):
+	prelog += 'No log directorie, create it: fail'
 	os.mkdir(os.getcwd()+'/log')
+	prelog = prelog[:len(prelog)-4]+'done\n'
 
+print(prelog)
 
 
 
