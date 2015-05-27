15d8b8ed71447d2473d4212e8f885bfda6bb4277
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 15:20:42 +0200

    change configuration directorie check method

--- main.py
+++ main.py
@@ -3,13 +3,23 @@
 
 import os
 
-#on vas dans le dossier dédié a blender manager
-os.chdir('/home/'+os.getlogin()+'/')
-try:
-	os.listdir(os.getcwd()).index('.BlenderRenderManager')
-except ValueError:
-	#si le dossier n'existe pas on le créer
-	os.mkdir('.BlenderRenderManager')
+#aller dans le dossier de configuration de Blender Render Manager
+if not os.path.exists('/home/'+os.getlogin()+'/.BlenderRenderManager'):
+	os.mkdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
 os.chdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
 
 
