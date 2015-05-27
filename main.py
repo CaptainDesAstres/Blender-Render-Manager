6548bb7f9445e0ee26e7018fb71111686cc407bf
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 15:23:35 +0200

    create log directorie if it don't exist

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ if not os.path.exists('/home/'+os.getlogin()+'/.BlenderRenderManager'):
 	os.mkdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
 os.chdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
 
-	
+#vérifier l'existance du dossier des logs et création si necessaire:
+if not os.path.exists(os.getcwd()+'/log'):
+	os.mkdir(os.getcwd()+'/log')
 
 
 
