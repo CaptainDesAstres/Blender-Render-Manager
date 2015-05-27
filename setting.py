da37633b73b633ee6161743eb422fe0bbf226e11
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 18:33:49 +0200

    create class to regroup settings

---
+++ setting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+class Log:
+	def __init__(self, xml= None):
+		self.x = 1920
+		self.y = 1080
+		self.percent = 1
+		
+		if xml is not None
+			self.parseXml(self,xml)
+
+	def parseXml(self,xml)
+		print('je ne sait pas encore parser!')
+
+
+
