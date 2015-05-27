3aef2745ef9ebfbeffa2fec990b2129553c02612
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 15:45:35 +0200

    add date and time in log file name

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
-
+import time
 import os
-prelog = 'ouverture de Blender Render Manager\n'
+
+start = time.strftime('%d/%m/%Y-%H:%M:%S')
+log = 'ouverture de Blender Render Manager\nSession du '+start+'\n'
+
 #aller dans le dossier de configuration de Blender Render Manager
 if not os.path.exists('/home/'+os.getlogin()+'/.BlenderRenderManager'):
-	prelog += 'No configuration directorie, create it: fail'
+	log += 'No configuration directorie, create it: fail'
 	os.mkdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
-	prelog = prelog[:len(prelog)-4]+'done\n'
+	log = log[:len(log)-4]+'done\n'
 else:
-	prelog += 'Find configuration directorie\n'
+	log += 'Find configuration directorie\n'
 os.chdir('/home/'+os.getlogin()+'/.BlenderRenderManager')
 
 #vérifier l'existance du dossier des logs et création si necessaire:
 if not os.path.exists(os.getcwd()+'/log'):
-	prelog += 'No log directorie, create it: fail'
+	log += 'No log directorie, create it: fail'
 	os.mkdir(os.getcwd()+'/log')
-	prelog = prelog[:len(prelog)-4]+'done\n'
+	log = log[:len(log)-4]+'done\n'
 
-print(prelog)
+print(log)
 
 
 
