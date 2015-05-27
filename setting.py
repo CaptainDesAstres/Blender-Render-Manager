b5f6c89d17913c8f7d7c5c67c295e43132ac2751
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-27 21:56:36 +0200

    add method that set settings from xml

--- setting.py
+++ setting.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
 
-class Log:
+class setting:
 	def __init__(self, xml= None):
 		self.x = 1920
 		self.y = 1080
 		self.percent = 1
 		
-		if xml is not None
-			self.parseXml(self,xml)
+		if xml is not None:
+			self.parseXml(xml)
 
-	def parseXml(self,xml)
-		print('je ne sait pas encore parser!')
+	def parseXml(self,xml):
+		node = xml.find('resolution')
+		self.x = node.get('x')
+		self.y = node.get('y')
+		self.percent = int(node.get('proportion'))/100
+		
 
 
 
