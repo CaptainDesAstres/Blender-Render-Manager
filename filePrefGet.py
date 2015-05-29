6eb7cf18e3e096881b91281fac955b5157e8e855
Captain DesAstres <thisistylerdurden@gmail.com>
2015-05-29 13:48:00 +0200

    correct a wrong blender python API path to the start and end frame value

--- filePrefGet.py
+++ filePrefGet.py
@@ -9,6 +9,6 @@ for name in bpy.data.scenes.keys():
 	scene = bpy.data.scenes[name]
 	print('\t<scene name="'+name+'">')
 	print('\t\t<resolution x="'+str(scene.render.resolution_x)+'" y="'+str(scene.render.resolution_y)+'" percent="'+str(scene.render.resolution_percentage)+'" />')
-	print('<animation start="'+str(scene.render.frame_start)+'" end="'+str(scene.render.frame_end)+'" fps="'+str(scene.render.fps)+'" />')
+	print('<animation start="'+str(scene.frame_start)+'" end="'+str(scene.frame_end)+'" fps="'+str(scene.render.fps)+'" />')
 	print('\t</scene>')
 print('</preferences>')
