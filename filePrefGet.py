3c3341654db6506ca2714f14f9dcbfe2ee776278
Captain DesAstres <thisistylerdurden@gmail.com>
2015-06-02 19:43:28 +0200

    add blender file parameter to extracted informations

--- filePrefGet.py
+++ filePrefGet.py
@@ -9,6 +9,32 @@ for name in bpy.data.scenes.keys():
 	scene = bpy.data.scenes[name]
 	print('\t<scene name="'+name+'">')
 	print('\t\t<resolution x="'+str(scene.render.resolution_x)+'" y="'+str(scene.render.resolution_y)+'" percent="'+str(scene.render.resolution_percentage)+'" />')
-	print('<animation start="'+str(scene.frame_start)+'" end="'+str(scene.frame_end)+'" fps="'+str(scene.render.fps)+'" />')
+	print('\t\t<animation start="'+str(scene.frame_start)+'" end="'+str(scene.frame_end)+'" fps="'+str(scene.render.fps)+'" />')
+	
+	
+	print('\t\t<engine engine="'+scene.render.engine+'" device="'+scene.cycles.device+'" ')
+	if scene.render.engine == 'CYCLES':
+		print('sample="'+str(scene.cycles.samples)+'" ')
+	print('/>')
+	
+	if scene.render.engine == 'CYCLES':
+		print('\t\t<film exposure="'+str(scene.cycles.film_exposure)+'" transparent="'+str(scene.cycles.film_transparent)+'" />')
+		print('\t\t<bouncesSet>')
+		print('\t\t\t<transparency min="'+str(scene.cycles.transparent_min_bounces)+'" max="'+str(scene.cycles.transparent_max_bounces)+'" />')
+		print('\t\t\t<bounces min="'+str(scene.cycles.min_bounces)+'" max="'+str(scene.cycles.max_bounces)+'" />')
+		print('\t\t\t<diffuse bounces="'+str(scene.cycles.diffuse_bounces)+'" />')
+		print('\t\t\t<glossy bounces="'+str(scene.cycles.glossy_bounces)+'" />')
+		print('\t\t\t<transmission bounces="'+str(scene.cycles.transmission_bounces)+'" />')
+		print('\t\t\t<volume bounces="'+str(scene.cycles.volume_bounces)+'" />')
+		print('\t\t</bouncesSet>')
+	
+	print('\t\t<output format="'+scene.render.image_settings.file_format+'" />')
+	
+	print('\t\t<renderLayerList>')
+	for renderLayerName in scene.render.layers.keys():
+		layer = scene.render.layers[renderLayerName]
+		print('\t\t\t<layer Z="'+str(layer.use_pass_z)+'" objIndex="'+str(layer.use_pass_object_index)+'" />')
+	print('\t\t</renderLayerList>')
+	
 	print('\t</scene>')
 print('</preferences>')
