3c116ec25442fca2b9b1cae91c86ffbd28a351cb
Captain DesAstres <thisistylerdurden@gmail.com>
2015-06-04 12:33:50 +0200

    add docstrings

--- render.py
+++ render.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
+'''module containing class 'render' '''
 from setting import setting
 
 class render:
+	'''class that contain the parameter for a rendering task'''
+	
 	def __init__(self,xml=False):
+		'''reder object initialisation
+		if there is no xml argument paste to the function, the status of the object is set to 'unset', path and scene is an empty strings and settings is a setting object with default value'''
 		self.path = ''
 		self.scene = ''
 		self.settings = setting()
@@ -13,6 +18,7 @@ class render:
 			self.fromXml(xml)
 	
 	def fromXml(self,xml):
+		'''method that set the object attributes with the value extracted from an xml object with 'render' tag name '''
 		if xml.tag == 'render':
 			self.path = xml.get('path')
 			self.scene = xml.get('scene')
@@ -20,6 +26,7 @@ class render:
 			self.status = 'ready'
 		
 	def toXmlStr(self,head=False):
+		'''export the object values into an xml formated strings'''
 		txt =''
 		if head:
 			txt+= '<?xml version="1.0" encoding="UTF-8"?>\n'
