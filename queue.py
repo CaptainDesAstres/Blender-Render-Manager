89b2321041447648a0d86075bf3e0e0901d1a3ef
Captain DesAstres <thisistylerdurden@gmail.com>
2015-06-04 13:42:45 +0200

    add docstrings and comments

--- queue.py
+++ queue.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 -*
+'''module that contain queue class'''
 from render import render
 
 class queue:
+	'''class who contain the list of all the rendering task to manage'''
 	def __init__(self,xml=False):
+		'''initialize queue object with empty queue who is filled with values extract from an xml object if paste to the function'''
 		self.renders = []
 		if xml != False:
 			self.fromXml(xml)
 	
 	def fromXml(self,xml):
+		'''extract rendering task parameters from an xml object and add them to the queue'''
 		if xml.tag == 'queue':
 			for r in xml.findall('render'):
 				self.add(render(r))
 		
 	def toXmlStr(self,head=False):
+		'''export rendering task queue to an xml syntax string '''
 		txt =''
 		if head:
 			txt+= '<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -24,6 +29,7 @@ class queue:
 		return txt
 	
 	def add(self,added):
+		'''add rendering task to the queue''' 
 		if type(added) == render:
 			self.renders.append(added)
 
