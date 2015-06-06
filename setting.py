c82e6c9b6c128bdfab8300d870dffc2be9f101ea
Captain DesAstres <captaindesastres@gmail.com>
2015-06-06 10:53:52 +0200

    change start/end animation frame default value

--- setting.py
+++ setting.py
@@ -10,8 +10,8 @@ class setting:
 		self.x = 1920
 		self.y = 1080
 		self.percent = 1
-		self.start = 0
-		self.end = 0
+		self.start = None
+		self.end = None
 		self.fps = 30
 		
 		if xml is not None:
@@ -28,9 +28,17 @@ class setting:
 		#get animation parameters
 		node = xml.find('animation')
 		if node is not None:
-			self.start = int(node.get('start',0))
-			self.end = int(node.get('end',0))
+			self.start = int(node.get('start',None))
+			self.end = int(node.get('end',None))
 			self.fps = int(node.get('fps',30))
+		self.startEndCheck()
+	
+	def startEndCheck(self):
+		'''make sure that that start frame and end frame are all set or all None'''
+		if self.start is None and self.end is not None:
+			self.start = self.end
+		elif self.start is not None and self.end is None:
+			self.end = self.start
 			
 	
 	def toXmlStr(self, head=False):
@@ -45,7 +53,7 @@ class setting:
 		txt+='<resolution x="'+str(self.x)+'" y="'+str(self.y)+'" percent="'+str(int(self.percent*100))+'" />\n'
 		
 		#export animation parameters depending of settings type
-		if self.start == 0 and self.end == 0:
+		if self.start is None or self.end is None:
 			txt+= '<animation fps="'+str(self.fps)+'" />\n'
 		else:
 			txt+= '<animation start="'+str(self.start)+'" end="'+str(self.end)+'" fps="'+str(self.fps)+'" />\n'
@@ -82,7 +90,7 @@ class setting:
 		
 		#write animation parameters
 		txt += 'animation : '
-		if self.start != 0 or self.end != 0 :
+		if self.start is not None and self.end is not None :
 			txt+= 'frame '+str(self.start)+' à '+str(self.end)+' '
 		txt+='(@'
 		
@@ -102,6 +110,7 @@ class setting:
 			clone.start = start
 		if end is not None:
 			clone.end = end
+		clone.startEndCheck()
 		return clone
 		
 
