cc13b55fc721037118e1340849eacdca7700551c
Captain DesAstres <captaindesastres@gmail.com>
2015-06-07 14:31:16 +0200

    import setting module and use it to improve the output of fileGetPref.py script

--- filePrefGet.py
+++ filePrefGet.py
@@ -3,6 +3,9 @@
 '''Script executed by Blender Python API for recoverring a blender file settings in XML format in standart output'''
 
 import bpy
+import sys, os
+sys.path.append(os.path.abspath(sys.argv[0]+'/..'))
+from setting import *
 
 print('<?xml version="1.0" encoding="UTF-8"?>')
 print('<preferences>')
@@ -11,34 +14,9 @@ for name in bpy.data.scenes.keys():
 	scene = bpy.data.scenes[name]
 	
 	print('\t<scene name="'+name+'">')
+	pref = setting()
 	
-	#get resolution and animation parameters
-	print('\t\t<resolution x="'+str(scene.render.resolution_x)+'" y="'+str(scene.render.resolution_y)+'" percent="'+str(scene.render.resolution_percentage)+'" />')
-	print('\t\t<animation start="'+str(scene.frame_start)+'" end="'+str(scene.frame_end)+'" fps="'+str(scene.render.fps)+'" />')
 	
-	if scene.render.engine == 'CYCLES':
-		#get Cycles specific parameters
-		print('\t\t<engine engine="'+scene.render.engine+'" device="'+scene.cycles.device+'" sample="'+str(scene.cycles.samples)+'" />')
-		print('\t\t<film exposure="'+str(scene.cycles.film_exposure)+'" transparent="'+str(scene.cycles.film_transparent)+'" />')
-		print('\t\t<bouncesSet>')
-		print('\t\t\t<transparency min="'+str(scene.cycles.transparent_min_bounces)+'" max="'+str(scene.cycles.transparent_max_bounces)+'" />')
-		print('\t\t\t<bounces min="'+str(scene.cycles.min_bounces)+'" max="'+str(scene.cycles.max_bounces)+'" />')
-		print('\t\t\t<diffuse bounces="'+str(scene.cycles.diffuse_bounces)+'" />')
-		print('\t\t\t<glossy bounces="'+str(scene.cycles.glossy_bounces)+'" />')
-		print('\t\t\t<transmission bounces="'+str(scene.cycles.transmission_bounces)+'" />')
-		print('\t\t\t<volume bounces="'+str(scene.cycles.volume_bounces)+'" />')
-		print('\t\t</bouncesSet>')
-	else:
-		print('\t\t<engine engine="'+scene.render.engine+'" device="'+scene.cycles.device+'" />')
-	
-	print('\t\t<output format="'+scene.render.image_settings.file_format+'" />')
-	
-	#get all Render Layer parameters
-	print('\t\t<renderLayerList>')
-	for renderLayerName in scene.render.layers.keys():
-		layer = scene.render.layers[renderLayerName]
-		print('\t\t\t<layer name="'+renderLayerName+'" z="'+str(layer.use_pass_z)+'" objIndex="'+str(layer.use_pass_object_index)+'" />')
-	print('\t\t</renderLayerList>')
-	
+	print('\t\t'+pref.toXmlStr().replace('\n','\n\t\t'))
 	print('\t</scene>')
 print('</preferences>')
