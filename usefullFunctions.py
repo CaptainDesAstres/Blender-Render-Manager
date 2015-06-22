2841ed211e27cff455f75aaa75e2ff61b3e6ee26
Captain DesAstres <captaindesastres@gmail.com>
2015-06-22 12:32:09 +0200

    create a module with fonctions that can be usefull in different module create a function to make sure that a string don't overtake a size limit

---
+++ usefullFunctions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+
+def columnLimit(value, limit, begin = True):
+	'''function to make sure to have a good column size'''
+	if type(value) is not str:
+		value = str(value)
+	
+	if begin is True:
+		begin = limit
+	
+	if len(value) > limit
+		return value[0:begin-1]+'…'+value[len(value)-(limit-begin)]+'|'
+	else:
+		return value+(' '*(limit-len(value)))+'|'
