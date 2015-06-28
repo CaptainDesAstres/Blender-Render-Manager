db689744216f19ed425e3155dd8d57d792497f4a
Captain DesAstres <captaindesastres@gmail.com>
2015-06-28 20:34:43 +0200

    create VersionList module and class to manage the list of blender version installed on user system

---
+++ settingMod/VersionList.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage list of all know version of Blender in the system'''
+import xml.etree.ElementTree as xmlMod
+
+
+class VersionList:
+	'''class dedicated to Blender version managing'''
+	
+	
+	def __init__(self, xml= None):
+		'''initialize Blender version list with default value or values extracted from an xml object'''
+		if xml is None:
+			self.defaultInit()
+		else:
+			self.fromXml(xml)
+	
+	
+	
+	
+	
+	def defaultInit(self):
+		'''initialize Blender version list with default value'''
+		
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize Blender version list with values extracted from an xml object'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export Blender version list into xml syntaxed string'''
+		return ''
+	
+	
+	
+	
+	
+	def see(self, log):
+		'''method to see Blender version list and access edition menu'''
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
