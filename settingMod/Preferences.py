31381ede1a5a7c72a4549e7050acde4b9d1d9156
Captain DesAstres <captaindesastres@gmail.com>
2015-06-28 20:00:19 +0200

    create a class Preferences to manage the preferences of the script

---
+++ settingMod/Preferences.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage preferences of the script'''
+import xml.etree.ElementTree as xmlMod
+
+
+class Preferences:
+	'''class dedicated to script preferences settings'''
+	
+	
+	def __init__(self, xml= None):
+		'''initialize preferences object with default value or values extracted from an xml object'''
+		
+	
+	
+	
+	
+	
+	def defaultInit(self):
+		'''initialize preferences object with default value'''
+		
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize preferences object with values extracted from an xml object'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export preferences settings into xml syntaxed string'''
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
