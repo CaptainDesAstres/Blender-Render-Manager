48f50b4a7620902845e208d2ffcca7d2a5ff99a5
Captain DesAstres <captaindesastres@gmail.com>
2015-06-29 17:33:05 +0200

    create Output module with basics output class structure

---
+++ settingMod/Output.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage rendering output path'''
+import xml.etree.ElementTree as xmlMod
+import re
+import os
+
+class Output:
+	'''class to manage rendering output path'''
+	
+	
+	def __init__(self, xml= None):
+		'''initialize output path with default value or values extracted from an xml object'''
+		if xml is None:
+			self.defaultInit()
+		else:
+			self.fromXml(xml)
+	
+	
+	
+	
+	
+	def defaultInit(self):
+		'''initialize output path with default value'''
+		
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize output path with values extracted from an xml object'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export output path into xml syntaxed string'''
+		
+	
+	
+	
+	
+	
+	def see(self, log):
+		'''method to see output path and access edition menu'''
+		
+	
+	
+	
+	
+	
+	def print(self, index = False, std = True):
+		'''a method to display the output path settings'''
+		
+	
