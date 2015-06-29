f7050b5f7bfe032cd5692fb8cb9aca1fe8406366
Captain DesAstres <captaindesastres@gmail.com>
2015-06-29 12:12:48 +0200

    create a script to get the version of a Blender build

---
+++ settingMod/getBlenderVersion.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''A script to be executed by Blender Python API to return Blender version'''
+
+import bpy
+print('<?xml version="1.0" encoding="UTF-8"?>')
+
+version = bpy.app.version_string
+print('<version version="'+str(version[0])+'.'+str(version[1])+'-'\
+					+str(bpy.app.build_branch)+'" />')
+
