870d30a0cb7788055cfc9c22854cdbe6293036fa
Captain DesAstres <captaindesastres@gmail.com>
2015-07-06 16:23:13 +0200

    create class to list preset and metapreset

---
+++ settingMod/PresetList.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage preset list'''
+import xml.etree.ElementTree as xmlMod
+import os
+
+class PresetList:
+	'''class to manage preset list'''
+	
+	
+	def __init__(self, xml= None):
+		'''initialize preset list with default value or values extracted from an xml object'''
+		if xml is None:
+			self.defaultInit()
+		else:
+			self.fromXml(xml)
+	
+	
+	
+	
+	
+	def defaultInit(self):
+		'''initialize preset list with default value'''
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize preset list with values extracted from an xml object'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export preset list into xml syntaxed string'''
+		txt = '<presetList>\n'
+		
+		txt += '</presetList>\n'
+		return txt
+	
+	
+	
+	
+	
+	def see(self, log, versions):
+		'''menu to explore and edit preset list settings'''
+		change = False
+		log.menuIn('preset list')
+		
+		while True:
+			os.system('clear')
+			log.print()
+			
+			self.print()
+			
+			print('''\n\n        Menu :
+0- Save and quit
+
+''')
+			
+			choice = input('Action?').strip().lower()
+			
+			if choice in ['0', 'q', 'quit', 'cancel']:
+				log.menuOut()
+				return change
+			else:
+				log.error('Unvalid menu choice', False)
+		
+	
+	
+	
+	
+	
+	def print(self):
+		'''a method to print preset list'''
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
