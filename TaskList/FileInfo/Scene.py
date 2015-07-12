0c7c96767d38a37b00c6243c3175f0525e71028f
Captain DesAstres <captaindesastres@gmail.com>
2015-07-12 19:19:30 +0200

    implement a class to manage scene info

---
+++ TaskList/FileInfo/Scene.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage blender scene info'''
+import xml.etree.ElementTree as xmlMod
+import os
+
+class Scene:
+	'''class to manage blender scene info'''
+	
+	
+	def __init__(self, xml):
+		'''initialize blender scene info with default settings or saved settings'''
+		self.fromXml(xml)
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize blender scene info with savedd settings'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export blender scene info into xml syntaxed string'''
+		xml = '<scene>'
+		
+		xml += '</scene>'
+		return xml
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
