7d1ad0c04bd3028af37afecc548cd1c66cb57c17
Captain DesAstres <captaindesastres@gmail.com>
2015-07-12 19:01:50 +0200

    implement a claas to manage file info

---
+++ TaskList/FileInfo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage blender file info'''
+import xml.etree.ElementTree as xmlMod
+import os
+
+class FileInfo:
+	'''class to manage blender file info'''
+	
+	
+	def __init__(self, xml):
+		'''initialize blender file info with default settings or saved settings'''
+		self.fromXml(xml)
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize blender file info with savedd settings'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export blender file info into xml syntaxed string'''
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
