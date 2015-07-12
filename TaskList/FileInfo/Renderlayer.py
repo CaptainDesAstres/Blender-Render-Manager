0ff42fbbec168f54dd93fd3e59a5da24a1bf80e3
Captain DesAstres <captaindesastres@gmail.com>
2015-07-12 19:30:43 +0200

    implement a class to manage renderlayer info

---
+++ TaskList/FileInfo/Renderlayer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage blender Renderlayer info'''
+import xml.etree.ElementTree as xmlMod
+import os
+
+class Renderlayer:
+	'''class to manage blender Renderlayer info'''
+	
+	
+	def __init__(self, xml):
+		'''initialize blender Renderlayer info with default settings or saved settings'''
+		self.fromXml(xml)
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize blender Renderlayer info with savedd settings'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export blender Renderlayer info into xml syntaxed string'''
+		return '<renderlayer />'
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
