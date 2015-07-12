e68b78f7564257787b4c173c0c9636408bba8695
Captain DesAstres <captaindesastres@gmail.com>
2015-07-12 18:34:16 +0200

    implement à script to get info on a blender file

---
+++ TaskList/getter/getFileTaskInfos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''Script executed by Blender Python API to export info on a blender file in XML format in standart output'''
+import bpy
+import sys, os, re
+
+print('<?xml version="1.0" encoding="UTF-8"?>')
+print('<fileInfo>')
+
+for name in bpy.data.scenes.keys():
+	scene = bpy.data.scenes[name]
+	
+	print('  <scene name="'+name+'" start="'+str(scene.frame_start)\
+			+'" end="'+str(scene.frame_end)+'" fps="'+scene.render.fps+'" >')
+	
+	for renderlayer in scene.render.layers.keys():
+		print('    <renderlayer name="'+renderlayer+'" use="'+str(scene.render.layers[name].use)+'" />')
+	
+	print('  </scene>')
+
+print('</fileInfo>')
