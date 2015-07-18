63d4eb641fb3c6e34d0bec84a829a0c18d8f0a22
Captain DesAstres <captaindesastres@gmail.com>
2015-07-18 12:29:36 +0200

    batch edit: copy: implement adding of copy at the end of the list

--- TaskList/TaskList.py
+++ TaskList/TaskList.py
@@ -626,10 +626,18 @@ Quit : q or quit
 		for t in copies:
 			t.preset = preset
 		
+		if row == 2:
+			newSelect = list(range(len(self.tasks),len(self.tasks) + len(copies)))
+			self.tasks += copies
+			log.write('Task n°«'+','.join(str(x) for x in select)+'» copied to row n°'+str(newSelect[0])+' to '+str(newSelect[-1])+' with '+preset+' preset')
+			log.menuOut()
+			return newSelect, True
+		else:
+			print()
 		
 		
 		log.menuOut()
-		return False
+		return select, False
 	
 	
 	
