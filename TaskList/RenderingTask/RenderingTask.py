ebdd37bfc93561388fab96fd12a09959cedaf051
Captain DesAstres <captaindesastres@gmail.com>
2015-07-29 13:15:34 +0200

    make sure to use the good start/end/fps settings and activate the wanted renderlayers

--- TaskList/RenderingTask/RenderingTask.py
+++ TaskList/RenderingTask/RenderingTask.py
@@ -4,3 +4,12 @@ import bpy
 def RenderingTask(task, preferences, groups, preset):
 	scene = bpy.data.scenes[task.scene]
 	bpy.context.screen.scene = scene
+	sceneInfo = task.info.scenes[task.scene]
+	scene.frame_start = sceneInfo.start
+	scene.frame_end = sceneInfo.end
+	scene.render.fps = sceneInfo.fps
+	
+	for name, RL in scene.render.layers.items():
+		RL.use = sceneInfo.renderlayers[name].use
+	
+	
