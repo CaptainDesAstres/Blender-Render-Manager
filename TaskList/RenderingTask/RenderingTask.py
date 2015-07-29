69385109c337788de35cf88680b236c734b385e8
Captain DesAstres <captaindesastres@gmail.com>
2015-07-29 13:30:05 +0200

    check if a preset or a metapreset is used

--- TaskList/RenderingTask/RenderingTask.py
+++ TaskList/RenderingTask/RenderingTask.py
@@ -1,7 +1,7 @@
 '''A module to manage task rendering in blender'''
-import bpy, sys
-sys.path.append("'''+scriptPath+'''")
-
+import bpy, sys, os
+sys.path.append(os.path.abspath(sys.argv[4]+'/../../../..'))
+from Preferences.PresetList.Preset.Preset import *
 
 def RenderingTask(task, preferences, groups, preset):
 	scene = bpy.data.scenes[task.scene]
@@ -14,4 +14,15 @@ def RenderingTask(task, preferences, groups, preset):
 	for name, RL in scene.render.layers.items():
 		RL.use = sceneInfo.renderlayers[name].use
 	
-	
+	if type(preset) is Preset:
+		print('\033[31mit\'s a preset!\033[0m')
+	else:
+		print('\033[31mit\'s a '+str(type(preset))+'!\033[0m')
+
+
+
+
+
+
+
+
