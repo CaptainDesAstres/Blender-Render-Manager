4aabc0aa265ab71bf1b65d98fc4b0fbf1114ff84
Captain DesAstres <captaindesastres@gmail.com>
2015-08-05 17:00:18 +0200

    prepare FrameLog class skeleton

---
+++ TaskList/TaskLog/FrameLog.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''module to manage task Frame log'''
+import xml.etree.ElementTree as xmlMod
+
+
+class TaskLog:
+	'''class to manage task frame log'''
+	
+	
+	def __init__(self, xml = None):
+		'''initialize task frame object'''
+		if xml is None:
+			self.defaultInit()
+		else:
+			self.fromXml(xml)
+	
+	
+	
+	
+	
+	def defaultInit(self):
+		'''initialize Task frame log object'''
+		
+	
+	
+	
+	
+	
+	def fromXml(self, xml):
+		'''initialize Task frame log object with saved log'''
+		
+	
+	
+	
+	
+	
+	def toXml(self):
+		'''export task frame log into xml syntaxed string'''
+		
+		
+	
+	
+	
+	
+	
+	def print(self):
+		'''A method to print task frame log'''
+		
+	
+	
+	
+	
+	
