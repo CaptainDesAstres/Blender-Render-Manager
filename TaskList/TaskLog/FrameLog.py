277ba1510fe078a9981f47d0bffa83b4137a7d8a
Captain DesAstres <captaindesastres@gmail.com>
2015-08-05 17:09:18 +0200

    default FrameLog initialization

--- TaskList/TaskLog/FrameLog.py
+++ TaskList/TaskLog/FrameLog.py
@@ -8,10 +8,14 @@ class TaskLog:
 	'''class to manage task frame log'''
 	
 	
-	def __init__(self, xml = None):
+	def __init__(self, xml = None, 
+					nb = None,
+					path = None,
+					date = None,
+					computingTime = None):
 		'''initialize task frame object'''
 		if xml is None:
-			self.defaultInit()
+			self.defaultInit(nb, path, date, computingTime)
 		else:
 			self.fromXml(xml)
 	
@@ -19,9 +23,12 @@ class TaskLog:
 	
 	
 	
-	def defaultInit(self):
+	def defaultInit(self, nb, path, date, computingTime):
 		'''initialize Task frame log object'''
-		
+		self.frame = nb
+		self.path = path
+		self.date = date
+		self.computingTime = computingTime
 	
 	
 	
